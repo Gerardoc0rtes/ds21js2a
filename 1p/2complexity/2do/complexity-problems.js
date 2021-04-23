03ac1147d649b3cb710444e7658103810a554ba7
Gerardoc0rtes <79116670+Gerardoc0rtes@users.noreply.github.com>
2021-04-23 01:09:27 -0500

    Update complexity-problems.js

--- 1p/2complexity/2do/complexity-problems.js
+++ 1p/2complexity/2do/complexity-problems.js
@@ -10,19 +10,52 @@
 // -> a integer
 // -> b integer
 // <- lcm 
+let num1,num2,mcm,i;
+  num1=prompt(' Ingrese el primer valor ' );
+  num1=parseInt(num1);
+  num2=prompt(' Ingrese el segundo valor ' );
+ num2=parseInt(num2);
+  mcm=1;
+  i=2;
+  
+  while(i <= num1 || i <= num2)
+        {
+            if(num1%i==0 || num2%i==0)
+            {
+            mcm=mcm*i;
+            if(num1%i==0) num1=num1/i;
+            if(num2%i==0) num2=num2/i;
+            }
+            else
+                i=i+1;      
+        }
+  console.log(' El Minímo Común Múltiplo es ' + mcm);
 
 //2. sieve of Eratosthenes
 //   sieve(n)
 // -> n integer
 // <- the n first elements
 
+
 //3. random number
 //   random(n)
 // -> n integer
 // <- one random number in [1-n]
+
 
 //4. random name
 //   randomName()
 // -> 
 // <- one random name
 // ie name371
+function random(n) {
+	return Math.floor(Math.random() * n + 1)
+}
+random(10)
+
+function randomName() {
+	let nombre = 'name'
+	return console.log(nombre + random(10))
+}
+random(10)
+randomName()

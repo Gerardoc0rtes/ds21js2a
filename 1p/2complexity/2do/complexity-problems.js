58951749ea7464c0d077818b69770ca66c622e64
Gerardoc0rtes <79116670+Gerardoc0rtes@users.noreply.github.com>
2021-04-23 01:25:59 -0500

    Update complexity-problems.js

--- 1p/2complexity/2do/complexity-problems.js
+++ 1p/2complexity/2do/complexity-problems.js
@@ -37,10 +37,14 @@ let num1,num2,mcm,i;
 // <- the n first elements
 
 
+
 //3. random number
 //   random(n)
 // -> n integer
 // <- one random number in [1-n]
+let random = parseInt(prompt('Introduce el limite del aleatorio'));
+let num = Math.floor(Math.random() * random) + 1;
+console.log('El numero aleatorio es: '+num)
 
 
 //4. random name

3a757d5787a661730a16a7b82cda29fbaeb9225c
Gerardoc0rtes <79116670+Gerardoc0rtes@users.noreply.github.com>
2021-04-23 01:28:46 -0500

    Update sorting-problems.js

--- 1p/3sorting/2do/sorting-problems.js
+++ 1p/3sorting/2do/sorting-problems.js
@@ -100,3 +100,14 @@ orden_Burbuja(num1)
 // -> o an object containing random names
 // <- the random names sorted
 // verify is sorted
+let A = Array.from({length: 10}, () => 
+String.fromCharCode(Math.floor(Math.random() * (90 - 65)) + 65)
++String.fromCharCode(Math.floor(Math.random() * (90 - 65)) + 65)
++String.fromCharCode(Math.floor(Math.random() * (90 - 65)) + 65)
++String.fromCharCode(Math.floor(Math.random() * (90 - 65)) + 65));
+console.log('Array inicial: '+ A)
+
+A.sort();
+
+console.log('Array acomodado: '+ A)
+

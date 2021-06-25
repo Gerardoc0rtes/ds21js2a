3645c2cefd827c8bd4ca67e2730845c385b2c1b2
Gerardoc0rtes <79116670+Gerardoc0rtes@users.noreply.github.com>
2021-06-25 16:10:10 -0500

    Update 0ds-problems.js

--- 2p/1vector/2do/0ds-problems.js
+++ 2p/1vector/2do/0ds-problems.js
@@ -15,7 +15,13 @@
  * @example
  * insertAt(3,3)
  * Verify operation result by outputing the array before and after
- *
+ 
+ *SOLUCION
+ 
+ let numbers = [ 4,2,1,6,8,3,9,7];
+    numbers.splice(3,0,'Número');
+    console.log(numbers);
+ 
  * PROBLEM 2
  * removeAt(p)
  *
@@ -24,7 +30,12 @@
  *
  * @example
  * removeAt(3)
- *
+ 
+ *SOLUCION
+let nums = [1,2,3,4,5,9,7],  pos=3, eliminar = nums.splice(pos, 1);
+    console.log(nums); 
+
+    
  * PROBLEM 3
  * insertFirst(v)
  *
@@ -35,7 +46,16 @@
  * insertFirst(4)
  * may wrap method unshift
  * Verify operation result by outputing the array before and after
- *
+
+*SOLUCION
+    let numeros = [2,3,4,5] , primero = 9 , N = numeros.length;
+    
+        for (let i = N; i >= 0; --i) {
+        numeros[i] = numeros[i-1];
+   }
+    numeros[0] =primero;
+    console.log(numeros); 
+ 
  * PROBLEM 4
  * removeLast()
  *
@@ -46,7 +66,13 @@
  * removeLast()
  * may wrap method pop
  * Verify operation result by outputing the array before and after
- *
+
+*SOLUCION
+
+let nums = [4,6,8,10,12];
+   nums.pop();
+   console.log(nums);
+   
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */

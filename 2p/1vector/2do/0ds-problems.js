dbcc5632356c945a9e1c685e1a3b9c6ccc94defb
Gerardoc0rtes <79116670+Gerardoc0rtes@users.noreply.github.com>
2021-06-25 16:22:42 -0500

    Update 0ds-problems.js

--- 2p/1vector/2do/0ds-problems.js
+++ 2p/1vector/2do/0ds-problems.js
@@ -1,11 +1,13 @@
-/** INSTRUCTIONS
- * 
+/* JESUS GERARDO CORTES VAZQUEZ
+
+//** INSTRUCTIONS
+ /*
  * create one file by program
  * implement a method, function, non-function solution
  * solve each as requested
  * may use code from previous exercises
  * 
- *//PROBLEM 1
+ *PROBLEM 1
  * insertAt(p,v)
  *
  * inserts a value v, at position p, in the array
@@ -16,13 +18,13 @@
  * insertAt(3,3)
  * Verify operation result by outputing the array before and after
  
- */SOLUCION
+ /*SOLUCION
+ */
+ let numeros = [ 4,2,1,6,8,3,9,7];
+    numbers.splice(3,0);
+    console.log(numeros);
  
- let numbers = [ 4,2,1,6,8,3,9,7];
-    numbers.splice(3,0,'Número');
-    console.log(numbers);
- 
- */ PROBLEM 2
+ /* PROBLEM 2
  * removeAt(p)
  *
  * removes value at position p
@@ -31,12 +33,13 @@
  * @example
  * removeAt(3)
  
- */SOLUCION
-let nums = [1,2,3,4,5,9,7],  pos=3, eliminar = nums.splice(pos, 1);
-    console.log(nums); 
+ SOLUCION
+ */
+let array = [0,2,4,6,8],  pos=4, eliminar = nums.splice(pos, 2);
+    console.log(array); 
 
     
- * /PROBLEM 3
+ /* PROBLEM 3
  * insertFirst(v)
  *
  * inserts value v at first position
@@ -47,7 +50,8 @@ let nums = [1,2,3,4,5,9,7],  pos=3, eliminar = nums.splice(pos, 1);
  * may wrap method unshift
  * Verify operation result by outputing the array before and after
 
-*/SOLUCION
+SOLUCION
+*/
     let numeros = [2,3,4,5] , primero = 9 , N = numeros.length;
     
         for (let i = N; i >= 0; --i) {
@@ -56,7 +60,7 @@ let nums = [1,2,3,4,5,9,7],  pos=3, eliminar = nums.splice(pos, 1);
     numeros[0] =primero;
     console.log(numeros); 
  
- * /PROBLEM 4
+ /* PROBLEM 4
  * removeLast()
  *
  * removes value at last position
@@ -67,12 +71,14 @@ let nums = [1,2,3,4,5,9,7],  pos=3, eliminar = nums.splice(pos, 1);
  * may wrap method pop
  * Verify operation result by outputing the array before and after
 
-*/SOLUCION
+SOLUCION
+*/
 
 let nums = [4,6,8,10,12];
    nums.pop();
    console.log(nums);
    
- * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
+ /* 
+  @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
